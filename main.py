210ea62e3a0931073cae36456f3d413b5f7ccde7
Sanah Rajesh <136401567+SanahR@users.noreply.github.com>
2024-09-10 19:32:49 -0500

    Adding Comments
    Putting in statistics, notes, and helpful information.

--- main.py
+++ main.py
@@ -18,11 +18,17 @@ x_train,x_test,y_train,y_test = train_test_split(iris.data,iris.target,test_size
 ##############################
 # CREATING/TRAINING THE MODEL#
 ##############################
+
+#Creating the model based on the best parameters from previous experimentation
+#Please note that model isn't actually needed, as y_pred could be created with clf.predict(x_test). I chose to do it this way because I prefer it, but it will run without creating the model separately. 
 model = MLP(hidden_layer_sizes = (5,5),max_iter = 7000,learning_rate_init = 0.01,activation = 'logistic')
 model.fit(x_train,y_train)
+
+#Creating the GridSearchCV
 clf = GridSearchCV(MLP(), param_grid = parameters,verbose = 1)
 clf.fit(x_train,y_train)
 
+#Printing out key information from the cross-validation
 print(clf.cv_results_.keys())
 print(clf.best_params_)
 print(clf.best_score_)
@@ -31,5 +37,9 @@ print(clf.best_score_)
 # PREDICTION AND TESTING     #
 ##############################
 y_pred = model.predict(x_test)
+#The average accuracy of this algorithm based on 20 runs is approximately 97.78 percent. 
+
+#Multiplying the accuracy score by 100 to get a percentage, then rounding that to clean the number up. 
 percentage = round((accuracy_score(y_test,y_pred)*100),2)
 print("The accuracy score of this algorithm was, ",percentage)
+
